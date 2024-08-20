788dda1101840361145a4ec9d3f20c04b54e7ca8
Francisco Sánchez Méndez <franthekillaz@gmail.com>
2024-08-20 18:09:59 -0400

    [SQUASH] Initial commit
    * https://github.com/Fraaxius/calculo_dv_run/commit/b15f221ec2913fb3b796eb015cd89001b32b9461
    * https://github.com/Fraaxius/calculo_dv_run/commit/7c92ade45532433d5e1d7805e3f5036283c0682a

---
+++ CalculoDV.py
@@ -0,0 +1,12 @@
+# Programa hecho por Francisco Sánchez M.
+
+# Declaración de variables
+run = 0
+runinvertido = 0
+
+print ('Hola! Bienvenido al programa de Cálculo de Dígito Verificador.\n')
+run=input("Ingresa el rut que deseas calcular: ")
+
+runinvertido = run[::-1]
+
+print (f"El RUN invertido es: {runinvertido}")
---
+++ README.md
@@ -0,0 +1,2 @@
+# calculo_dv_run
+Programa para calcular el dígito verificador de un RUN chileno.
