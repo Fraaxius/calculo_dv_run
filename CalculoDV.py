d6cd8c282882a877dd86098efa2e975558681e1d
Fraaxius <franthekillaz@gmail.com>
2024-08-21 00:50:09 -0400

    CalculoDV: Initial bringup with logical procedure
    * TEST: Working to calculate some chilean RUNs.
    
    Signed-off-by: Fraaxius <franthekillaz@gmail.com>

--- CalculoDV.py
+++ CalculoDV.py
@@ -1,14 +1,42 @@
-# Programa hecho por Francisco Sánchez M.
+from datetime import datetime
+import time
 
-# [WIP]
+# Programa creado por Francisco Sánchez M.
 
-# Declaración de variables
+# Variables
 run = 0
 runinvertido = 0
+dv = ''
 
 print ('Hola! Bienvenido al programa de Cálculo de Dígito Verificador.\n')
-run=input("Ingresa el rut que deseas calcular: ")
+run=input("Ingresa el RUN que deseas calcular (sin dìgito verificador, ejemplo: 21123456): ")
 
-runinvertido = run[::-1]
+# Verificación de input
+if not run.isdigit():
+    print("\nError: La entrada no es válida. Verifica si escribiste el RUN con el formato antes mencionado.")
+else:
+    runinvertido = run[::-1]
+    print ('Calculando...\n')
+    time.sleep(3)
 
-print (f"El RUN invertido es: {runinvertido}")
+    # Secuencia de multiplicación
+    multiplicacion = [2, 3, 4, 5, 6, 7]
+
+    # Cálculo de suma de las multiplicaciones
+    suma = 0
+    for i in range(len(runinvertido)):
+        digito = int(runinvertido[i])
+        peso = multiplicacion[i % len(multiplicacion)]
+        suma += digito * peso
+    
+    # Calculo de dígito verificador (finalmente)
+    residuo = suma % 11
+    dv = 11 - residuo
+
+    if dv == 11:
+        dv = 0
+    elif dv == 10:
+        dv = 'K'
+    
+    print(f"El digito verificador del RUN es: {dv}")
+    print(f"Gracias por utilizar el programa!")
